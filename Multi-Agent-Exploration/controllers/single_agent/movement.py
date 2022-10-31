90397a57d6a84e321475ec6f27688b5fc3888cab
darren-chiu <darrenchiu321@ghmail.com>
2022-10-31 01:01:15 -0400

    Moved files

--- Multi-Agent-Exploration/controllers/single_agent/movement.py
+++ Multi-Agent-Exploration/controllers/single_agent/movement.py
@@ -1,21 +1,22 @@
 #LIBRARY IMPORTS
-from controller import Robot
-from controller import Supervisor
-from controller import Compass
+from gettext import translation
 import math
 import numpy as np
 
 
 class Movement:
-    def __init__(self, robot, motorL, motorR, position):
+    def __init__(self, robot, motorL, motorR, compass, gyro):
         self.robot = robot
         self.motorL = motorL
-        self.motorR = motorR 
-        self.position = position
+        self.motorR = motorR
+        self.compass = compass
+        self.gyro = gyro
         #DEFINED CONSTANTS
-        COORDINATE_MATCHING_ACCURACY = 0.01
+    COORDINATE_MATCHING_ACCURACY = 0.01
     THETA_MATCHING_ACCURACY = 1
-    ROBOT_ANGULAR_SPEED_IN_DEGREES = 18
+    # ANGULAR SPEED IN RADIANS 32778.0
+    ROBOT_ANGULAR_SPEED_IN_DEGREES = 42
+    #Linear velocity 0.0549999930
     TANGENSIAL_SPEED = 0.128
     MAX_SPEED = 10
 
@@ -24,170 +25,169 @@ class Movement:
         self.motorR.setVelocity(0)
 
     def motorMoveForward(self):
-        print("moving forward")
-        print(self.position)
         self.motorL.setVelocity(self.MAX_SPEED)
         self.motorR.setVelocity(self.MAX_SPEED)
 
-    # def motorRotateLeft():
-    #     motorL.setVelocity(-MAX_SPEED)
-    #     motorR.setVelocity(MAX_SPEED)
+    def motorRotateLeft(self):
+        self.motorL.setVelocity(-self.MAX_SPEED)
+        self.motorR.setVelocity(self.MAX_SPEED)
 
-    # def motorRotateRight():
-    #     motorL.setVelocity(MAX_SPEED)
-    #     motorR.setVelocity(-MAX_SPEED)
+    def motorRotateRight(self):
+        self.motorL.setVelocity(self.MAX_SPEED)
+        self.motorR.setVelocity(-self.MAX_SPEED)
 
-    # def moveForward(distance):
-    #     # the duration required for the robot to move by the specified distance
-    #     duration = distance / TANGENSIAL_SPEED
-    #     print("duration to reach target location: %.5f\n", duration)
+    def moveForward(self, distance):
+        # the duration required for the robot to move by the specified distance
+        duration = distance / self.TANGENSIAL_SPEED
+        print("duration to reach target location: %.5f\n", duration)
 
-    #     #set robot motor to move forward
-    #     motorMoveForward()
+        #set robot motor to move forward
+        self.motorMoveForward()
 
-    #     #run the simulator
-    #     start_time = robot.getTime()
-    #     while True:
-    #         if (robot.getTime() >= start_time + duration):
-    #             break
-    #         robot.step(8)
-    #     #stop the motor
-    #     motorStop()
-    #     robot.step(8)
+        #run the simulator
+        start_time = self.robot.getTime()
+        while True:
+            if (self.robot.getTime() >= start_time + duration):
+                break
+            self.robot.step(8)
+        #stop the motor
+        self.motorStop()
+        self.robot.step(8)
 
-    # def rotateHeading(thetaDot):
-    #     if not (cartesianIsThetaEqual(thetaDot, 0)):
-    #         duration = abs(thetaDot) / ROBOT_ANGULAR_SPEED_IN_DEGREES
-    #         print("duration to face the destination: %.5f\n", duration)
+    def rotateHeading(self, thetaDot):
+        if not (self.cartesianIsThetaEqual(thetaDot, 0)):
+            duration = abs(thetaDot) / self.ROBOT_ANGULAR_SPEED_IN_DEGREES
+            print("duration to face the destination: %.5f\n", duration)
 
-    #         #if thetaDot > 0, robot will rotate to left
-    #         if (thetaDot > 0):
-    #             # set robot motor to rotate left
-    #             motorRotateLeft()
-    #         #if thetaDot < 0, robot will rotate to right
-    #         elif (thetaDot < 0):
-    #             # set robot motor to rotate right
-    #             motorRotateRight()
+            #if thetaDot > 0, robot will rotate to left
+            if (thetaDot > 0):
+                # set robot motor to rotate left
+                self.motorRotateLeft()
+            #if thetaDot < 0, robot will rotate to right
+            elif (thetaDot < 0):
+                # set robot motor to rotate right
+                self.motorRotateRight()
 
-    #         # run the simulator
-    #         start_time = robot.getTime()
-    #         while True:
-    #                 if (robot.getTime() >= start_time + duration):
-    #                     break
-    #                 robot.step(8)
+            # run the simulator
+            start_time = self.robot.getTime()
+            while True:
+                    #print(self.gyro.getValues())
+                    if (self.robot.getTime() >= start_time + duration):
+                        break
+                    self.robot.step(8)
 
-    # #motorR.setPosition(0.1)
-    # # Main loop:
-    # # - perform simulation steps until Webots is stopping the controller
-    # def cartesianConvertCompassBearingToHeading(heading):
-    #     # /* 
-    #     # * in webots, heading increasement is rotate in clockwise
-    #     # * in cartesian, heading increasement is rotate in counterclockwise
-    #     # * so headingInCartesian = 360-headingInWebots
-    #     # * */
-    #     heading = 360-heading
+    #motorR.setPosition(0.1)
+    # Main loop:
+    # - perform simulation steps until Webots is stopping the controller
+    def cartesianConvertCompassBearingToHeading(self, heading):
+        # /* 
+        # * in webots, heading increasement is rotate in clockwise
+        # * in cartesian, heading increasement is rotate in counterclockwise
+        # * so headingInCartesian = 360-headingInWebots
+        # * */
+        heading = 360-heading
         
-    #     # /* 
-    #     # * in webots, heading is 0 if robot faced to y+ axis
-    #     # * in cartesian, heading is 0 if robot face to x+ axis
-    #     # * so headingInCartesian = headingInWebots+90
-    #     # * */
-    #     heading = heading + 90
-    #     if (heading > 360.0):
-    #         heading = heading - 360.0
+        # /* 
+        # * in webots, heading is 0 if robot faced to y+ axis
+        # * in cartesian, heading is 0 if robot face to x+ axis
+        # * so headingInCartesian = headingInWebots+90
+        # * */
+        heading = heading + 90
+        if (heading > 360.0):
+            heading = heading - 360.0
 
-    #     return heading
+        return heading
 
 
-    # def positioningControllerGetRobotCoordinate():
-    #     position = np.zeros(2)
-    #     position[0] = (trans_field_array[0].getSFVec3f())[0]
-    #     position[1] = -(trans_field_array[0].getSFVec3f())[2]
+    def positioningControllerGetRobotCoordinate(self, translation_array):
+        position = np.zeros(2)
+        position[0] = (translation_array[0])
+        position[1] = -(translation_array[2])
         
-    #     return position   
+        return position   
         
-    # def cartesianIsCoordinateEqual(coordinate1, coordinate2):
-    #     if (abs(coordinate1[0]-coordinate2[0]) < COORDINATE_MATCHING_ACCURACY and abs(coordinate1[1]-coordinate2[1]) < COORDINATE_MATCHING_ACCURACY):
-    #         return True
-    #     else:
-    #         return False
+    def cartesianIsCoordinateEqual(self, coordinate1, coordinate2):
+        if (abs(coordinate1[0]-coordinate2[0]) < self.COORDINATE_MATCHING_ACCURACY and abs(coordinate1[1]-coordinate2[1]) < self.COORDINATE_MATCHING_ACCURACY):
+            return True
+        else:
+            return False
     
-    # def cartesianIsThetaEqual(theta, theta2):
-    #     if (abs(theta - theta2) < THETA_MATCHING_ACCURACY):
-    #         return True
-    #     else:
-    #         return False
+    def cartesianIsThetaEqual(self, theta, theta2):
+        if (abs(theta - theta2) < self.THETA_MATCHING_ACCURACY):
+            return True
+        else:
+            return False
             
-    # def get_bearing_in_degrees():
-    #     north = compass.getValues()
-    #     rad = math.atan2(north[1], north[0])
-    #     bearing = (rad - 1.5708) / math.pi * 180.0
-    #     if (bearing < 0.0):
-    #         bearing = bearing + 360.0
-    #     return bearing
+    def get_bearing_in_degrees(self):
+        north = self.compass.getValues()
+        rad = math.atan2(north[1], north[0])
+        bearing = (rad - 1.5708) / math.pi * 180.0
+        if (bearing < 0.0):
+            bearing = bearing + 360.0
+        return bearing
 
 
-    # def positioningControllerGetRobotHeading():
-    #     return cartesianConvertCompassBearingToHeading(get_bearing_in_degrees())
+    def positioningControllerGetRobotHeading(self):
+        heading = self.get_bearing_in_degrees()
+        return self.cartesianConvertCompassBearingToHeading(heading)
         
-    # def cartesianCalcDestinationThetaInDegrees(currentCoordinate,  destinationCoordinate):
-    #     return math.atan2(destinationCoordinate[1] - currentCoordinate[1], destinationCoordinate[0] - currentCoordinate[0]) * 180 / math.pi
+    def cartesianCalcDestinationThetaInDegrees(self, currentCoordinate,  destinationCoordinate):
+        return math.atan2(destinationCoordinate[1] - currentCoordinate[1], destinationCoordinate[0] - currentCoordinate[0]) * 180 / math.pi
 
 
-    # def cartesianCalcThetaDot(heading, destinationTheta):
-    #     theta_dot = destinationTheta - heading;
+    def cartesianCalcThetaDot(self, heading, destinationTheta):
+        theta_dot = destinationTheta - heading;
 
-    #     if (theta_dot > 180):
-    #         theta_dot = -(360-theta_dot)
-    #     elif (theta_dot < -180):
-    #         theta_dot = (360+theta_dot)
+        if (theta_dot > 180):
+            theta_dot = -(360-theta_dot)
+        elif (theta_dot < -180):
+            theta_dot = (360+theta_dot)
 
-    #     return theta_dot
+        return theta_dot
 
-    # def cartesianCalcDistance(currentCoordinate, destinationCoordinate):
-    #     return math.sqrt(math.pow(destinationCoordinate[0]-currentCoordinate[0], 2) + math.pow(destinationCoordinate[1]-currentCoordinate[1], 2))
-
-
-    # def positioningControllerCalcDistanceToDestination(destinationCoordinate):
-
-    #     currentCoordinate = positioningControllerGetRobotCoordinate()
-    #     return cartesianCalcDistance(currentCoordinate, destinationCoordinate)
-
-    # def positioningControllerCalcThetaDotToDestination(destinationCoordinate):
-
-    #     currentCoordinate = positioningControllerGetRobotCoordinate()
-    #     robotHeading = positioningControllerGetRobotHeading()
-    #     destinationTheta = cartesianCalcDestinationThetaInDegrees(currentCoordinate, destinationCoordinate)
-    #     return cartesianCalcThetaDot(robotHeading, destinationTheta)
+    def cartesianCalcDistance(self, currentCoordinate, destinationCoordinate):
+        return math.sqrt(math.pow(destinationCoordinate[0]-currentCoordinate[0], 2) + math.pow(destinationCoordinate[1]-currentCoordinate[1], 2))
 
 
+    def positioningControllerCalcDistanceToDestination(self, destinationCoordinate, curr_pos):
 
-    # def moveToDestination(destinationCoordinate):
-    #     currentCoordinate = positioningControllerGetRobotCoordinate()
-    #     print("Initial Coordinate: %.5f %.5f\n", currentCoordinate[0], currentCoordinate[1])
-    #     print("Destination Coordinate: %.5f %.5f\n", destinationCoordinate[0], destinationCoordinate[1])
+        currentCoordinate = self.positioningControllerGetRobotCoordinate(curr_pos)
+        return self.cartesianCalcDistance(currentCoordinate, destinationCoordinate)
+
+    def positioningControllerCalcThetaDotToDestination(self, destinationCoordinate, curr_pos):
+
+        currentCoordinate = self.positioningControllerGetRobotCoordinate(curr_pos)
+        robotHeading = self.positioningControllerGetRobotHeading()
+        destinationTheta = self.cartesianCalcDestinationThetaInDegrees(currentCoordinate, destinationCoordinate)
+        return self.cartesianCalcThetaDot(robotHeading, destinationTheta)
+
+
+
+    def moveToDestination(self, destinationCoordinate, curr_pos):
+        currentCoordinate = self.positioningControllerGetRobotCoordinate(curr_pos)
+        # print("Initial Coordinate: {2.5f} {2.5f}\n".format(currentCoordinate[0], currentCoordinate[1]))
+        # print("Destination Coordinate: {2.5f} {2.5f}\n", destinationCoordinate[0], destinationCoordinate[1])
         
-    #     # if the robot is already at the destination location
-    #     if (cartesianIsCoordinateEqual(positioningControllerGetRobotCoordinate(), destinationCoordinate)):
+        # if the robot is already at the destination location
+        if (self.cartesianIsCoordinateEqual(self.positioningControllerGetRobotCoordinate(curr_pos), destinationCoordinate)):
+                print("Robot is already at the destination location\n")
+                return
             
-    #             printf("Robot is already at the destination location\n")
-    #             return
-            
-    #     # thetaDot is the degree of rotation needed by the robot to face the destination
-    #     # thetaDot is zero if robot is facing the destination
+        # thetaDot is the degree of rotation needed by the robot to face the destination
+        # thetaDot is zero if robot is facing the destination
 
-    #     thetaDotToDestination = positioningControllerCalcThetaDotToDestination(destinationCoordinate)
-    #     print("thetaDotToDestination: %.5f\n", thetaDotToDestination)
+        thetaDotToDestination = self.positioningControllerCalcThetaDotToDestination(destinationCoordinate, curr_pos)
+        # print("thetaDotToDestination: %.5f\n", thetaDotToDestination)
 
-    #     rotateHeading(thetaDotToDestination)
+        self.rotateHeading(thetaDotToDestination)
 
-    #     # the distance needed for the robot to reach its destination
-    #     distanceToDestination = positioningControllerCalcDistanceToDestination(destinationCoordinate)
-    #     print("distanceToDestination: %.5f\n", distanceToDestination)
+        # the distance needed for the robot to reach its destination
+        distanceToDestination = self.positioningControllerCalcDistanceToDestination(destinationCoordinate, curr_pos)
+        # print("distanceToDestination: %.5f\n", distanceToDestination)
         
-    #     moveForward(distanceToDestination)
+        self.moveForward(distanceToDestination)
         
-    #     currentCoordinate = positioningControllerGetRobotCoordinate()
-    #     print("Stop Coordinate: %.5f %.5f\n", currentCoordinate[0], currentCoordinate[1])
+        currentCoordinate = self.positioningControllerGetRobotCoordinate(curr_pos)
+        # print("Stop Coordinate: %.5f %.5f\n", currentCoordinate[0], currentCoordinate[1])
 
 
--- Multi-Agent-Exploration/controllers/single_agent/single_agent.py
+++ Multi-Agent-Exploration/controllers/single_agent/single_agent.py
@@ -1,13 +1,16 @@
 """single_agent controller."""
 
 # You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 from controller import Supervisor
 from controller import Compass
+from controller import Gyro
+import sys
+import platform
 import math
 import numpy as np
-import igraph as ig
+# import igraph as ig
 from movement import Movement
 
 num_robots = 1
@@ -22,26 +25,36 @@ motorL = robot.getDevice("left motor")
 motorR = robot.getDevice("right motor")
 
 compass = Compass("compass")
+gyro = Gyro("gyro")
 
 motorL.setPosition(float('inf'))
 motorR.setPosition(float('inf'))
 # motorL.setPosition(0.1)
-defArray=["agent_1"]
+defVal="agent_1"
 rov_node_array = np.empty(num_robots, dtype=object)
 trans_field_array = np.empty(num_robots, dtype=object)
 trans_value_array = np.empty(num_robots, dtype=object)
 
-for i in range(num_robots):
-    rov_node_array[i] = robot.getFromDef(defArray[i])
-    trans_field_array[i] = rov_node_array[i].getField("translation")
-    trans_value_array[i] = trans_field_array[i].getSFVec3f()
-    compass.enable(8)
-    mvController = Movement(robot, motorL, motorR, trans_value_array[i])
+agent_node = robot.getFromDef(defVal)
+trans_field = agent_node.getField("translation")
+trans_value = trans_field.getSFVec3f()
+compass.enable(8)
+gyro.enable(8)
+mvController = Movement(robot, motorL, motorR, compass, gyro)
 
-destination=[0.024, -0.156]
-mvController.motorMoveForward()
+# for i in range(num_robots):
+#     trans_field_array[i] = rov_node_array[i].getField("translation")
+#     trans_value_array[i] = trans_field_array[i].getSFVec3f()
+#     compass.enable(8)
+    
 
+destination=[-0.0256, -0.112]
+print(sys.version)
+print(platform.python_version())
 while robot.step(timestep) != -1:
+    #Update velocity after every simulation step
+    curr_pos = trans_field.getSFVec3f()
+    #print(agent_node.getVelocity())
     # motorRotateRight()
     # Read the sensors:
     # Enter here functions to read sensor data, like:
@@ -52,6 +65,7 @@ while robot.step(timestep) != -1:
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
     mvController.motorMoveForward()
+    #mvController.moveToDestination(destination, curr_pos)
     
 
 # Enter here exit cleanup code.
